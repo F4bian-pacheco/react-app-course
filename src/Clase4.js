b9de3daa5513698cde8b2280ec058ecb68143eee
fabian pacheco <fabian@gmail.com>
2021-11-30 01:11:50 -0300

    evitar tener estados que no uses

--- src/Clase4.js
+++ src/Clase4.js
@@ -3,16 +3,44 @@ import {useState} from 'react';
 
 
 const Clase4 = () =>{
-    const [left, setleft] = useState(0);
-    const [right, setright] = useState(0);
+    //const [left, setleft] = useState(0); // mejores opciones
+    //const [right, setright] = useState(0);
+
+    const [counters, setCounters] = useState({
+        left: 0,
+        right: 0,
+        mensaje: "Mensaje en el estado"
+    });
+    const [clicks, setClicks] = useState([]);
+
+    const handleClickLeft = () => {
+        const newcounterState = {
+            ...counters,
+            left: counters.left + 1,
+        }
+        setCounters(newcounterState);
+        setClicks(prevclicks => [...prevclicks,"L"]);
+    };
+
+    const handleClickRight = () => {
+        const newcounterState = {
+            ...counters,
+            right: counters.right + 1,
+        }
+        setCounters(newcounterState);
+        setClicks(prevclicks => [...prevclicks,"R"]);
+    };
 
     return(
         <div>
             <h1>Clase 4: Manejando estados mas complejos </h1> 
-            {left}
-            <button onClick={()=> setleft(left+1)} >Left</button>
-            <button onClick={()=> setright(right+1)} >Right</button>
-            {right}
+            {counters.left}
+            <button onClick={handleClickLeft} >Left</button>
+            <button onClick={handleClickRight} >Right</button>
+            {counters.right}
+            <p>Clicks totales: {clicks.length}</p>
+            {counters.mensaje} <br/>
+            {clicks.join(", ")}
         </div>
     );
 };
