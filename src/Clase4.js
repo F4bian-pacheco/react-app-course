013d5f0b9c6d3c93a8968a4ddfbee647746b4eea
Fabian Andres <fabian.paxe@gmail.com>
2022-02-15 22:03:49 -0300

    add reset_counters function

--- src/Clase4.js
+++ src/Clase4.js
@@ -42,15 +42,28 @@ const Clase4 = () =>{
         setClicks(prevclicks => [...prevclicks,"R"]);
     };
 
+    const reset_counters = () => {
+        const newResetState = {
+            left: 0,
+            right: 0,
+            mensaje: "Reseteado"
+
+        }
+        setCounters(newResetState);
+        setClicks([]);
+    }
+
     return(
         <div>
             <h1>Clase 4: Manejando estados mas complejos </h1> 
+            <br />
             {counters.left}
             <button onClick={handleClickLeft} >Left</button>
             <button onClick={handleClickRight} >Right</button>
             {counters.right}
             <p>Clicks totales: {clicks.length}</p>
             {counters.mensaje} <br/>
+            <button onClick={reset_counters}>Reset</button>
             {clicks.length === 0 ? (
                 <Nousado/>
             ): (
