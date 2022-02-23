4bf3f2875c6e909abe5c722ab9fd563cfd417b1b
Fabian Andres <fabian.paxe@gmail.com>
2022-02-23 01:15:13 -0300

    first part done

--- src/Clase6.js
+++ src/Clase6.js
@@ -1,11 +1,44 @@
 import { Post } from "./Post";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 
+// const posts_s = [
+//     {
+//         userId: 1,
+//         id: 1,
+//         title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//         body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+//     },
+//     {
+//         userId: 1,
+//         id: 2,
+//         title: "qui est esse",
+//         body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+//     },
+//     {
+//         userId: 1,
+//         id: 3,
+//         title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+//         body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+//     }
+// ]
 
-export default function Clase6(props) {
-    const [posts, setPost] = useState(props.posts) // mostrar posts
+
+export default function Clase6() {
+    const [posts, setPost] = useState([]) // mostrar posts
     const [newPost, SetNewPost] = useState("") // añadir post
+    const [Loading,SetLoading] = useState(false)
+
+    useEffect(() => {
+        SetLoading(true)
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+                .then((json) => {
+                    const {data} = json
+                    setPost(data)
+                    SetLoading(false)
+                })
+    }, [])
 
     const HandleChange = (event) => {
         const newpost = event.target.value
@@ -29,6 +62,7 @@ export default function Clase6(props) {
         <div>
             <h1>Clase 6: 📡 Fetching y Mutación de Datos en React con el hook useEffect</h1>
             <div>
+                {Loading ? "Cargando...": ""}
                 <h2>Posts</h2>
                 <ol id="list_notes">
                     {/* No es una buena practica */}
@@ -40,7 +74,7 @@ export default function Clase6(props) {
                 {/* Por defecto, en un formulario, el ultimo boton
                     funciona como un submit*/}
                 <form onSubmit={handleSubmit}>
-                    <input onChange={HandleChange} type ="text" value={newPost} />
+                    <input onChange={HandleChange} type="text" value={newPost} />
                     <button>Crear Post</button>
                 </form>
             </div>
